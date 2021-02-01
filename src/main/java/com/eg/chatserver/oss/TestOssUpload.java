d0b62464aab8b4886d88cfeab79b7760cc6262b6
bq <gOtQWGG4wZ>
2021-02-01 17:33:01 +0800

    阿里云啊阿里云

--- src/main/java/com/eg/chatserver/oss/TestOssUpload.java
+++ src/main/java/com/eg/chatserver/oss/TestOssUpload.java
@@ -13,9 +13,9 @@ public class TestOssUpload {
         String bucketName = "chat-oss-bucket";
 
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-        String accessKeyId = "STS.NUQREqkb7o7p8JPhCXDCq6Zzh";
-        String accessKeySecret = "5EYKeTQDZDDbptitbeN7eYpoBeDXsm8eCm7vVEiEZaVB";
-        String securityToken = "CAISiwJ1q6Ft5B2yfSjIr5bkGf/Fhr0W2PWbOmzhjEMNSMxd2Z/Rijz2IHlKfXdoAe0fsvQ/nW1Y6fcTlrh+W4NIX0rNaY5t9ZlN9wqkbtIIWz45cPpW5qe+EE2/VjTZvqaLEcibIfrZfvCyESOm8gZ43br9cxi7QlWhKufnoJV7b9MRLGLaBHg8c7UwHAZ5r9IAPnb8LOukNgWQ4lDdF011oAFx+wgdgOadupTCs0qB3AGgmrdF+Nivf8ieApMybMslYbCcx/drc6fN6ilU5iVR+b1+5K4+om6Z54/FWwQIv0XWareErYw+NnxwYqkrBqhDt+Pgkv51vOPekYntwgpKJ/tSVynPjnVWGEokgIkagAGVSbK6JtTjS9jXL91quB9sKaLxPjIxyr4QASCCltrJGYqrqscck22GpGjGapMiswEl2ndVCglcSVVPrLPMO1Oi0GNT6wFNr0kVmhpEisZ7U9W9XzJM17G3aeueEVTHuBltXQiMZha5e00OyWkkKtGh3YvCkd1tPxyEGl+oAn3FPg==";
+        String accessKeyId = "STS.NTsGvyy3EC52s2EwKBVVqoxtY";
+        String accessKeySecret = "CGZxSHvRs84w2vJtQz95pWH7FSrh8SBKGpbDAuJHTEDF";
+        String securityToken = "CAISiwJ1q6Ft5B2yfSjIr5fGDMzNlOxk9PfZcRT0k0sXWtldgL3fuzz2IHlKfXdoAe0fsvQ/nW1Y6fcTlrh+W4NIX0rNaY5t9ZlN9wqkbtI6Ykk6cPpW5qe+EE2/VjTZvqaLEcibIfrZfvCyESOm8gZ43br9cxi7QlWhKufnoJV7b9MRLGLaBHg8c7UwHAZ5r9IAPnb8LOukNgWQ4lDdF011oAFx+wgdgOadupTCs0qB3AGgmrdF+Nivf8ieApMybMslYbCcx/drc6fN6ilU5iVR+b1+5K4+om6Z54/FWwQIv0XWareErYw+NnxwYqkrBqhDt+Pgkv51vOPekYntwgpKJ/tSVynPjnVWGEokgIkagAEUc590Lz5PjZIwrLNH7B6RcL0CrkVG6dUwHNsz+URtpXWnohnKkWUJNoX6vAbk1OZyGXvhvhLKXdQEjqk0rbxIrJRiuZ4wQOzKNTzNjw0a/iBHjuIeiAimXJ8QwCi3V838NEGkDaLHIDxywezqYvYAvLTtZnpYhLB7KmuIR/l8kA==";
 
         // 用户拿到STS临时凭证后，通过其中的安全令牌（SecurityToken）和临时访问密钥（AccessKeyId和AccessKeySecret）生成OSSClient。
         // 创建OSSClient实例。注意，这里使用到了上一步生成的临时访问凭证（STS访问凭证）。
